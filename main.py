ba7f6d20f3e7ce8e255b791d62f79f00fa94abcb
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-31 20:40:08 -0400

    Add code to scrape data from table

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import pandas as pd
+
+url = "https://www.nibtt.net/Contribution_Rates/cont_Sept05_2016.htm"
+data = requests.get(url)
+
+# This parses all the tables in webpages to a list
+df_list = pd.read_html(data.text) 
+df = df_list[0]
+print(df.head())
